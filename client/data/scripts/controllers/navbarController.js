ba2acca045edc39c9853faeb67f2e5171a653a9b
Ruslan Razuvailo <razrus@hotmail.com>
2020-08-27 17:27:38 +0300

    Date time position in navbar.

--- client/data/scripts/controllers/navbarController.js
+++ client/data/scripts/controllers/navbarController.js
@@ -9,5 +9,8 @@
         
         // display date and time in navbar
         this.currentDateTime = dateTimeService.dateTime;
+
+        // set class for position of datetime in navbar
+        navbarService.setClass("mr-3 ml-auto");
     });
 }());
