d78e912fc25a9c4f802b68a93bbf719ea6c94a88
Ruslan Razuvailo <razrus@hotmail.com>
2020-08-29 11:25:50 +0300

    Change logic.

--- studying-schedule/Controllers/SignInController.cs
+++ studying-schedule/Controllers/SignInController.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Server.IIS.Core;
 using studying_schedule.Models.BaseUser;
+using studying_schedule.Models.SignIn;
 using studying_schedule.Database.SELECT;
 
 namespace studying_schedule.Controllers
@@ -21,13 +24,13 @@ namespace studying_schedule.Controllers
         {
             if (ModelState.IsValid)
             {
-                int userRole = SelectData.ValidateUser(user);
-
-                if (userRole != -1)
+                SignInModel model = SelectData.ValidateUser(user);
+                
+                if (model != null)
                 {
                     return new ContentResult
                     {
-                        Content = $"{userRole}",
+                        Content = JsonSerializer.Serialize(model),
                         ContentType = "application/json",
                         StatusCode = (int)HttpStatusCode.OK
                     };
