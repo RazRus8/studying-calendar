320a13a80f5fcb6bf9ababe03c9805c0ccf428f0
Ruslan Razuvailo <razrus@hotmail.com>
2020-08-31 17:41:08 +0300

    Rename.

---
+++ studying-schedule/Models/GroupModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace studying_schedule.Models
+{
+    [Table("groups")]
+    public class GroupModel
+    {
+        [Key]
+        [Column("groupId")]
+        public int GroupId { get; set; }
+
+        [Required]
+        [Column("groupName")]
+        public string GroupName { get; set; }
+    }
+}
