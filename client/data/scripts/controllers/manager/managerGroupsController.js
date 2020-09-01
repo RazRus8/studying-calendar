af3d1b19a24f804ae96361042f36eb6f7a161459
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-01 11:42:42 +0300

    Add funtionality to select on manager groups page.

--- client/data/scripts/controllers/manager/managerGroupsController.js
+++ client/data/scripts/controllers/manager/managerGroupsController.js
@@ -2,9 +2,9 @@
 {
     "use strict";
 
-    app.controller("managerGroupsController", function(navbarService, managerSelectService)
+    app.controller("managerGroupsController", function($route, navbarService, managerSelectService, managerGroupService)
     {
-        console.log("manager students page controller is working");
+        console.log("manager groups page controller is working");
 
         // enable icons in navbar
         navbarService.setIcons("/data/views/icons.html");
@@ -18,9 +18,53 @@
         // set select for hours and minutes
         this.time = managerSelectService.setTime();
 
+        // set active tab
         navbarService.setTab1("tab1");
         navbarService.setTab2("tab2-active");
         navbarService.setTab3("tab3");
         navbarService.setTab4("tab4");
+
+        // add new group to the db
+        this.createGroup = function()
+        {
+            var newGroup = 
+            {
+                GroupName: this.group
+            }
+
+            console.log(newGroup);
+
+            var promiseObj = managerGroupService.createGroup(newGroup);
+
+            promiseObj.then(function(value)
+            {
+                console.log("Response status: ", value.status);
+
+                if (value.status == 200)
+                {
+                    $route.reload();
+                }
+            });
+        }
+
+        this.groups = managerGroupService.getGroups();
+
+        this.deleteGroup = function()
+        {
+            var id = parseInt(this.value);
+            var group = {GroupId: id};
+
+            var promiseObj = managerGroupService.deleteGroup(group);
+
+            promiseObj.then(function(value)
+            {
+                console.log("Response status: ", value.status);
+
+                if (value.status == 200)
+                {
+                    $route.reload();
+                }
+            });
+        }
     });
 }());
---
+++ client/data/scripts/services/manager/managerGroupService.js
@@ -0,0 +1,77 @@
+(function()
+{
+    "use strict";
+
+    app.service("managerGroupService", function($q, $http)
+    {
+        this.createGroup = function(newGroup)
+        {
+            var deferred = $q.defer();
+
+            $http({
+                method: "POST",
+                data: newGroup,
+                url: "http://localhost:50157/api/creategroup"
+            })
+            .then(function success(response)
+            {
+                deferred.resolve(response);
+            },
+            function error(response)
+            {
+                deferred.reject(response);
+            });
+
+            return deferred.promise;
+        }
+
+        this.getGroups = function()
+        {
+            var deferred = $q.defer();
+            var groups = {init: null};
+
+            $http({
+                method: "GET",
+                url: "http://localhost:50157/api/getgroups"
+            })
+            .then(function success(response)
+            {
+                deferred.resolve(response);
+            },
+            function error(response)
+            {
+                deferred.reject(response);
+            });
+
+            var promiseObj = deferred.promise;
+            
+            promiseObj.then(function(value)
+            {
+                groups["groups"] = value.data;
+            });
+
+            return groups;
+        }
+
+        this.deleteGroup = function(group)
+        {
+            var deferred = $q.defer();
+
+            $http({
+                method: "POST",
+                data: group,
+                url: "http://localhost:50157/api/deletegroup"
+            })
+            .then(function success(response)
+            {
+                deferred.resolve(response);
+            },
+            function error(response)
+            {
+                deferred.reject(response);
+            });
+
+            return deferred.promise;
+        }
+    });
+}());
