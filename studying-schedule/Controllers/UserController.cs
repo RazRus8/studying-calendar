b404085713b8bb7b81ef3cb47ece6305fa55653c
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-02 11:45:42 +0300

    Select all lecturers.

---
+++ studying-schedule/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using studying_schedule.Database.SELECT;
+
+namespace studying_schedule.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [Route("api/getlecturers")]
+        [HttpGet]
+        public ContentResult GetLecturers()
+        {
+            var lecturers = SelectUser.SelectAllLecturers();
+
+            return new ContentResult
+            {
+                Content = JsonSerializer.Serialize(lecturers),
+                ContentType = "application/json",
+                StatusCode = (int)HttpStatusCode.OK
+            };
+        }
+    }
+}
---
+++ studying-schedule/Database/SELECT/SelectUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using studying_schedule.Models;
+
+namespace studying_schedule.Database.SELECT
+{
+    public class SelectUser
+    {
+        public static List<UserModel> SelectAllLecturers()
+        {
+            try
+            {
+                using (AppContext db = new AppContext())
+                {
+                    return db.UsersSet.Where(user => user.Role == 2).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+    }
+}
