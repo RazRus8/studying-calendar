b714e2af4ef825778add798e51fbb273b7638cd5
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-09 14:50:38 +0300

    Add folder with models of user and without.

---
+++ studying-schedule/Models/Info/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace studying_schedule.Models.Info
+{
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+
+        public int Month { get; set; }
+
+        public int Year { get; set; }
+    }
+}
