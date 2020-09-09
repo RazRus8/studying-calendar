33feeefd018eba43d17520ca62e63b30056f2e11
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-09 12:35:48 +0300

    Change user setter.

--- client/data/scripts/controllers/signInController.js
+++ client/data/scripts/controllers/signInController.js
@@ -36,19 +36,19 @@
                 if (value.status == 200 && value.data.Role == 1)
                 {
                     console.log("User role: student.", "User:", value.data.FirstName, value.data.LastName, "User id:", value.data.Id);
-                    userService.setUser(value.data);
+                    userService.setUser(value.data.Id);
                     $location.url("/home/student/" + value.data.Id);
                 }
                 else if (value.status == 200 && value.data.Role == 2)
                 {
                     console.log("User role: lecturer.", "User:", value.data.FirstName, value.data.LastName, "User id:", value.data.Id);
-                    userService.setUser(value.data);
+                    userService.setUser(value.data.Id);
                     $location.url("/home/lecturer/" + value.data.Id);
                 }
                 else if (value.status == 200 && value.data.Role == 3)
                 {
                     console.log("User role: manager.", "User:", value.data.FirstName, value.data.LastName, "User id:", value.data.Id);
-                    userService.setUser(value.data);
+                    userService.setUser(value.data.Id);
                     $location.url("/home/manager");
                 }
                 
