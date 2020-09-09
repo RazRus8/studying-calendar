28c4e2ea777e03a0a0af63535b4f87f993ccbb0e
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-09 20:06:08 +0300

    Add select for work with students and lecturers.

--- studying-schedule/Database/SELECT/SelectSchedule.cs
+++ studying-schedule/Database/SELECT/SelectSchedule.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using studying_schedule.Models;
 using studying_schedule.Models.Info;
 
@@ -10,21 +11,63 @@ namespace studying_schedule.Database.SELECT
     {
         public static List<FullScheduleModel> SelectFor(MonthYearModel monthYear)
         {
-            var fullInfoSchedules = new List<FullScheduleModel>();
-
             try
             {
                 using (AppContext db = new AppContext())
                 {
                     var schedules = db.ScheduleSet.Where(year => year.LectureDateTimeStart.Year == monthYear.Year)
-                                                 .Where(month => month.LectureDateTimeStart.Month == monthYear.Month)
-                                                 .ToList();
+                                                  .Where(month => month.LectureDateTimeStart.Month == monthYear.Month)
+                                                  .ToList();
 
+                    return Convert(schedules);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public static List<FullScheduleModel> SelectForUser(UserInfo user)
+        {
+            try
+            {
+                using (AppContext db = new AppContext())
+                {
+                    var groupId = db.StudentsGroupsSet.Where(g => g.StudentId == user.UserId).Select(g => g.GroupId).FirstOrDefault();
+                    
+                    if (groupId != 0)
+                    {
+                        var schedules = db.ScheduleSet.Where(year => year.LectureDateTimeStart.Year == user.Year)
+                                                      .Where(month => month.LectureDateTimeStart.Month == user.Month)
+                                                      .Where(g => g.StudentsGroup == groupId)
+                                                      .ToList();
+
+                        return Convert(schedules);
+                    }
+
+                    return null;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        private static List<FullScheduleModel> Convert(List<ScheduleModel> schedules)
+        {
+            try
+            {
+                var fullInfoSchedules = new List<FullScheduleModel>();
+
+                using (AppContext db = new AppContext())
+                {
                     foreach (ScheduleModel model in schedules)
                     {
                         string fullName = db.UsersSet.Where(user => user.Id == model.Lecturer)
-                                                 .Select(x => ($"{x.FirstName} {x.LastName}"))
-                                                 .FirstOrDefault();
+                                                     .Select(x => ($"{x.FirstName} {x.LastName}"))
+                                                     .FirstOrDefault();
 
                         string group = db.GroupsSet.Where(group => group.GroupId == model.StudentsGroup)
                                                    .Select(x => x.GroupName)
@@ -49,7 +92,7 @@ namespace studying_schedule.Database.SELECT
                     return fullInfoSchedules;
                 }
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 throw new Exception(ex.ToString());
             }
