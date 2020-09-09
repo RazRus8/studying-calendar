872f1f72f202dc39f4e2f82fbb987fdf7167cc67
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-09 12:37:59 +0300

    Initialize user service.

--- client/data/scripts/controllers/navbarController.js
+++ client/data/scripts/controllers/navbarController.js
@@ -2,7 +2,7 @@
 {
     "use strict";
 
-    app.controller("navbarController", function(navbarService, dateTimeService)
+    app.controller("navbarController", function(navbarService, dateTimeService, userService)
     {
         // initialize for using in other controllers
         this.navbarService = navbarService;
@@ -12,5 +12,8 @@
 
         // set class for position of datetime in navbar
         navbarService.setClass("mr-3 ml-auto");
+
+        // initialize for using in signin controller and on the tabs
+        this.user = userService;
     });
 }());
