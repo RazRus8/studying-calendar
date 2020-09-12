4a8f3617535a4d8a10828007774101f6cd3f9d26
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-12 21:51:27 +0300

    Add SelectAll method.

--- studying-schedule/Database/SELECT/SelectGroup.cs
+++ studying-schedule/Database/SELECT/SelectGroup.cs
@@ -7,6 +7,21 @@ namespace studying_schedule.Database.SELECT
 {
     public class SelectGroup
     {
+        public static List<GroupModel> SelectAll()
+        {
+            try
+            {
+                using (AppContext db = new AppContext())
+                {
+                    return db.GroupsSet.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+
         public static List<GroupModel> SelectAvailable()
         {
             try
