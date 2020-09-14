945e3f48cc5ac7bfecc9a90414309c7e9f5d314d
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-14 22:40:38 +0300

    Add model for password change.

---
+++ studying-schedule/Models/Info/ChangePasswordModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace studying_schedule.Models.Info
+{
+    public class ChangePasswordModel
+    {
+        public int Id { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
