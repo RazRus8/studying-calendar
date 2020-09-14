c58e03bc3492670314079380eec91a395c13c21d
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-14 13:03:45 +0300

    Create safe model.

---
+++ studying-schedule/Models/Info/SafeUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace studying_schedule.Models.Info
+{
+    public class SafeUserModel
+    {
+        public int Id { get; set; }
+
+        public byte Role { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+    }
+}
