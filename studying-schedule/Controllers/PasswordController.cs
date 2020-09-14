8375e6cedb8029f275d5d1762f9a2fc5705ed44c
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-14 22:50:36 +0300

    Add PasswordController.

---
+++ studying-schedule/Controllers/PasswordController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using studying_schedule.Database.SELECT;
+using studying_schedule.Database.UPDATE;
+using studying_schedule.Models.Info;
+
+namespace studying_schedule.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    public class PasswordController : ControllerBase
+    {
+        [Route("api/changepassword")]
+        [HttpPost]
+        public ContentResult ChangePassword(ChangePasswordModel user)
+        {
+            bool result = UpdatePassword.Update(user);
+
+            if (result)
+            {
+                return new ContentResult
+                {
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            else
+            {
+                return new ContentResult
+                {
+                    StatusCode = (int)HttpStatusCode.NotAcceptable
+                };
+            }
+        }
+    }
+}
