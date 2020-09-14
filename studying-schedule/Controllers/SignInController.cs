8dba6f958b4f26a4a7ec1971d1780a1a12294aca
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-14 22:49:54 +0300

    Delete unused directives. Change Validation to SelectPassword.

--- studying-schedule/Controllers/SignInController.cs
+++ studying-schedule/Controllers/SignInController.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Server.IIS.Core;
 using studying_schedule.Database.SELECT;
 using studying_schedule.Models;
 
@@ -23,7 +17,7 @@ namespace studying_schedule.Controllers
         {
             if (ModelState.IsValid)
             {
-                UserModel model = Validation.ValidateUser(user);
+                UserModel model = SelectPassword.ValidateUser(user);
                 
                 if (model != null)
                 {
