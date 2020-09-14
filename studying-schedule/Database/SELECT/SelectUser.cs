32d5bad010fa7fa9866040a58c4e8c0531a8c623
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-14 15:13:51 +0300

    Return SafeUserModel instead of UserModel.

--- studying-schedule/Database/SELECT/SelectUser.cs
+++ studying-schedule/Database/SELECT/SelectUser.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using studying_schedule.Models;
+using studying_schedule.Models.Info;
 
 namespace studying_schedule.Database.SELECT
 {
     public class SelectUser
     {
-        public static List<UserModel> SelectAllTeachers()
+        public static List<SafeUserModel> SelectAllTeachers()
         {
             try
             {
                 using (AppContext db = new AppContext())
                 {
-                    return db.UsersSet.Where(user => user.Role == 2).ToList();
+                    return db.UsersSet.Where(user => user.Role == 2)
+                                      .ToList()
+                                      .Select(teacher => new SafeUserModel
+                                      { 
+                                          Id = teacher.Id,
+                                          Role = teacher.Role,
+                                          FirstName = teacher.FirstName,
+                                          LastName = teacher.LastName
+                                      }).ToList();
                 }
             }
             catch (Exception ex)
