39b6ffca093f7826966ff8a1358f44e460f30005
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-14 13:01:23 +0300

    Add insert class for student controller.

---
+++ studying-schedule/Database/INSERT/InsertStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using studying_schedule.Models;
+
+namespace studying_schedule.Database.INSERT
+{
+    public class InsertStudent
+    {
+        public static void Insert(StudentsGroupsModel model)
+        {
+            try
+            {
+                using (AppContext db = new AppContext())
+                {
+                    db.StudentsGroupsSet.Add(model);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+    }
+}
