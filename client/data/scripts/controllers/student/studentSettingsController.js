6a2a3158b73fe222022fff8025af8c0fba1a9cbc
Ruslan Razuvailo <razrus@hotmail.com>
2020-09-15 15:10:16 +0300

    Add function for password changing.

--- client/data/scripts/controllers/student/studentSettingsController.js
+++ client/data/scripts/controllers/student/studentSettingsController.js
@@ -2,7 +2,7 @@
 {
     "use strict";
 
-    app.controller("studentSettingsController", function(navbarService)
+    app.controller("studentSettingsController", function($route, navbarService, userService, passwordService)
     {
         console.log("student page controller is working");
 
@@ -18,5 +18,81 @@
         // set active tab
         navbarService.setTab1("tab1");
         navbarService.setTab2("tab2-active");
+
+        this.changePassword = function()
+        {
+            var oldpass1 = document.getElementById("stud-old-pass-1");
+            var oldpass2 = document.getElementById("stud-old-pass-2");
+            var newpass = document.getElementById("stud-new-pass");
+
+            if (this.oldPassword1 !== undefined && this.oldPassword2 !== undefined && this.newPassword)
+            {
+                if (whiteSpaceCheck(this.oldPassword1))
+                {
+                    oldpass1.setCustomValidity(messages[2]);
+                }
+                else if (whiteSpaceCheck(this.oldPassword2))
+                {
+                    oldpass2.setCustomValidity(messages[2]);
+                }
+                else if (whiteSpaceCheck(this.newPassword))
+                {
+                    newpass.setCustomValidity(messages[2]);
+                }
+                
+                if (this.oldPassword1 == this.oldPassword2)
+                {
+                    if (this.oldPassword1 != this.newPassword)
+                    {
+                        var user =
+                        {
+                            Id: parseInt(userService.getUser().Id),
+                            OldPassword: this.oldPassword1,
+                            NewPassword: this.newPassword
+                        }
+    
+                        var promiseObj = passwordService.changePassword(user);
+    
+                        promiseObj.then(function(value)
+                        {
+                            if (value.status != 200)
+                            {
+                                alert("Incorrect old password.");
+                            }
+                            else
+                            {
+                                $route.reload();
+                                alert("Password successfully changed.");
+                            }
+                        });
+                    }
+                    else
+                    {
+                        newpass.setCustomValidity(messages[3]);
+                    }
+                }
+                else
+                {
+                    oldpass2.setCustomValidity(messages[1]);
+                }
+            }
+        }
+
+        var whiteSpaceCheck = function(string)
+        {
+            if (string.indexOf(" ") >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        var messages =
+        {
+            1 : "Second password does not match the first one.",
+            2 : "Password may contain letters, digits, special sybols and no whitespace.",
+            3 : "New password cannot be the same as the old one."
+        }
     });
 }());
